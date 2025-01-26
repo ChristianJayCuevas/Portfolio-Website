43e43d6c4a8b1c432fa05d3805549278eb718c7b
ChristianJayCuevas <christianjaycuevas@gmail.com>
2025-01-26 17:48:46 +0800

    Change Tab Name

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-const appName = import.meta.env.VITE_APP_NAME || 'CJay';
+const appName = 'CJay';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
